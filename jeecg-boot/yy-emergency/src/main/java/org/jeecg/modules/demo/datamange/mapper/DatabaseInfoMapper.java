c9b37488f62d7392368ce5ec536ac2a79f4a3a28
yeyuan <153961671@qq.com>
2021-07-30 20:13:12 +0800

    B 数据管理-基础数据库页面初始接口

---
+++ jeecg-boot/yy-emergency/src/main/java/org/jeecg/modules/demo/datamange/mapper/DatabaseInfoMapper.java
@@ -0,0 +1,17 @@
+package org.jeecg.modules.demo.datamange.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.jeecg.modules.demo.datamange.entity.DatabaseInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 基础数据库信息表
+ * @Author: jeecg-boot
+ * @Date:   2021-07-30
+ * @Version: V1.0
+ */
+public interface DatabaseInfoMapper extends BaseMapper<DatabaseInfo> {
+
+}
---
+++ jeecg-boot/yy-emergency/src/main/java/org/jeecg/modules/demo/datamange/service/IDatabaseInfoService.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.demo.datamange.service;
+
+import org.jeecg.modules.demo.datamange.entity.DatabaseInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: 基础数据库信息表
+ * @Author: jeecg-boot
+ * @Date:   2021-07-30
+ * @Version: V1.0
+ */
+public interface IDatabaseInfoService extends IService<DatabaseInfo> {
+
+}
